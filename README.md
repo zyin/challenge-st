76a5003fe1c4292657e48eed9829ea2e2105e4f5
Zhuhua Yin <zhuhua@Mac.lan>
2026-03-31 20:32:36 -0500

    First version of the challenge.
    Cover all code path.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # challenge-st
+
+* uv is installed - recommended
+  * uv sync
+  * uv run main.py
+
+* uv is not installed, python3 must be installed
+python3 main.py
---
+++ main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+def sort(width: int, height: int, length: int, mass: int) -> str:
+    result = ("STANDARD", "SPECIAL", "REJECTED")
+    LENGTH_LIMIT = 150           # UNIT = cm
+    MASS_LIMIT = 20              # UNIT = kg
+    DIMENSION_LIMIT = 1_000_000  # UNIT = cm^3
+
+    # The width, height, length, and mass of the package must be positive integers. If any of these values is zero or negative, the function should raise a ValueError with the message "Negative value is not allowed".
+    if width <= 0 or height <= 0 or length <= 0 or mass <= 0:
+        raise ValueError("Negative or zero value is not allowed")
+
+    volume = width * height * length
+    idx = 0
+    if volume >= DIMENSION_LIMIT or width >= LENGTH_LIMIT or height >= LENGTH_LIMIT or length >= LENGTH_LIMIT:
+        idx = 1
+
+    if mass >= MASS_LIMIT:
+        idx += 1
+
+    return result[idx]
+
+def main():
+    data = [10, 10, 10, 10]
+    try:
+        print(sort(*data))
+    except ValueError as e:
+        print(e)
+
+    data = [10, 10, 10, 10]
+    try:
+        print(sort(*data))
+    except ValueError as e:
+        print(e)
+
+    data = [0, 10, 10, 10]
+    try:
+        print(sort(*data))
+    except ValueError as e:
+        print(e)
+
+    data = [10, 10, 10, 0]
+    try:
+        print(sort(*data))
+    except ValueError as e:
+        print(e)
+
+    data = [10, 10, 10, 20]
+    try:
+        print(sort(*data))
+    except ValueError as e:
+        print(e)
+
+    data = [150, 10, 10, 10]
+    try:
+        print(sort(*data))
+    except ValueError as e:
+        print(e)
+ 
+    data = [150, 10, 10, 20]
+    try:
+        print(sort(*data))
+    except ValueError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
